fbf59ef7263e4495597a606ffba7d12e2baee8d8
Louis Groux <louis.cal.groux@gmail.com>
2025-01-24 16:51:42 +0000

    Fix CLI generation logic

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -7,8 +7,8 @@ if __name__ == '__main__':
     if '{{ cookiecutter.create_author_file }}' != 'y':
         pathlib.Path('AUTHORS.rst').unlink()
 
-    if 'no' in '{{ cookiecutter.command_line_interface|lower }}':
+    if 'none' in '{{ cookiecutter.command_line_interface|lower }}':
         pathlib.Path('src', '{{ cookiecutter.project_slug }}', '__main__.py').unlink()
-        
+
     if 'Not open source' == '{{ cookiecutter.open_source_license }}':
         pathlib.Path('LICENSE').unlink()
