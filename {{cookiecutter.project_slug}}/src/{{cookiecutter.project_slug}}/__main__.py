207883819bc46d9052a5152714c75b00af7e3b87
Louis Groux <louis.cal.groux@gmail.com>
2025-01-24 16:40:28 +0000

    Remove app.command

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/__main__.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/__main__.py
@@ -1,6 +1,5 @@
 import sys
 
-@app.command()
 def main():
     """Console script for {{cookiecutter.project_slug}}."""
     print("Edit src/{{cookiecutter.project_slug}}/__main__.py to extend CLI.")
